c4a01ffba5d8d20b2e13c5dcf4d00cfc06456b5e
Sahil Rajput <sahilrajput03@gmail.com>
2023-12-14 20:19:46 +0530

    Add benchamrk code for serial and parallel.

--- examples/examples-nodejs/faceRecognition.ts
+++ examples/examples-nodejs/faceRecognition.ts
@@ -34,8 +34,27 @@ async function run() {
 
   saveFile('referenceImage.jpg', (outRef as any).toBuffer('image/jpeg'))
 
+  // resultsQuery.forEach(fd => {
+  //   const bestMatch = faceMatcherRef.findBestMatch(fd.descriptor)
+  //   console.log("this is best match thing for query image -  ", bestMatch.toString())
+  // })
+
+  // console.log('---------------------');
+  // console.log('---------------------');
+  // console.log('---------------------');
+  // console.log('---------------------');
+  
   const queryDrawBoxes = resultsQuery.map(res => {
     const bestMatch = faceMatcher.findBestMatch(res.descriptor)
+    // console.log('bestMatch.toString()?', bestMatch.toString());
+    // OUPUT:
+    // bestMatch.toString()? person 4 (0.53)
+    // bestMatch.toString()? unknown (0.86)
+    // bestMatch.toString()? person 1 (0.46)
+    // bestMatch.toString()? person 2 (0.46)
+    // bestMatch.toString()? unknown (0.89)
+    // bestMatch.toString()? unknown (0.76)
+
     return new faceapi.draw.DrawBox(res.detection.box, { label: bestMatch.toString() })
   })
   const outQuery = faceapi.createCanvasFromMedia(queryImage)
@@ -44,4 +63,40 @@ async function run() {
   console.log('done, saved results to out/queryImage.jpg')
 }
 
-run()
+async function mainSequential(){
+  let results: any = []
+
+  for (let i = 0; i < 10; i++) {
+    var begin=Date.now();
+    await run()
+    var end= Date.now();
+    
+    var timeSpent=(end-begin)/1000+"secs";
+    results.push(timeSpent)
+  }
+  console.log('results?', results);
+}
+async function mainParallel(){
+  let results: any = []
+
+  async function runFaceReconition() {
+    var begin=Date.now();
+    await run()
+    var end= Date.now();
+    
+    var timeSpent=(end-begin)/1000+"secs";
+    results.push(timeSpent)
+  }
+  const promiseArray: any = []
+  for (let i = 0; i < 10; i++) {
+    promiseArray.push(runFaceReconition())
+  }
+
+  await Promise.all(promiseArray)
+  
+  console.log('results?', results);
+}
+
+// Note: Please run accordingly as per benchmark need
+mainSequential()
+// mainParallel()
